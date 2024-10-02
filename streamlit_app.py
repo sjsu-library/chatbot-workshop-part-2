73983320db54dfd5badc9d687cb5b37467dc4c5e
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-10-02 15:29:13 -0700

    Change system prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,9 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=0.2,
-        system_prompt="""You are a an expert on the work of Rabindrath Tagore, and you love to use quotations from his booksto illustrate your points.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
-        Respond using a florid but direct tone, typical of an early modernist writer.
-        Keep your answers under 100 words.""",
+        system_prompt="""You are a an expert on King Library's Digital Humanities Center.
+        Using the provided documents, respond with answers relevant to the Digital Humanities Center.
+        Use a supportive tone and use emojis when approporiate.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
     {
